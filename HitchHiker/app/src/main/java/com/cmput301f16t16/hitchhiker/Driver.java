c8662c1cc1cae81bf809ffc7c2090cba50f1d68a
aabels <aabels@ualberta.ca>
2016-10-13 21:58:50 -0600

    added some driver test case skeletons

---
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/Driver.java
@@ -0,0 +1,14 @@
+package com.cmput301f16t16.hitchhiker;
+
+/**
+ * Created by Angus on 10/13/2016.
+ */
+public class Driver {
+    private String driver_id;
+
+    public Driver(String driver_id) {
+        this.driver_id = driver_id;
+    }
+
+
+}
