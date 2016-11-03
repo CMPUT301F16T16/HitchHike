a9e4409fbb6fd58c1087f3c8d6db558074b8bf1e
liaowilly12123 <liaowilly@hotmail.com>
2016-11-03 00:55:16 -0600

    added RiderTest

--- HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/Rider.java
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/Rider.java
@@ -3,22 +3,10 @@ package com.cmput301f16t16.hitchhiker;
 /**
  * Created by Jae-yeon on 10/14/2016.
  */
-public class Rider {
-    private Integer riderID;
+public class Rider extends User{
 
-    public Rider(int RiderID) {
-        this.riderID = riderID;
+    public Rider(String username, String firstName, String lastName, String phoneNumber) {
+        super(username, firstName, lastName, phoneNumber);
     }
 
-    public int getRiderID() {
-        return this.riderID;
-    }
-
-    public void setRiderID(Integer riderID) {
-        this.riderID = riderID;
-    }
-
-    public int getRiderId() {
-        return this.riderID;
-    }
 }
--- HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/User.java
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/User.java
@@ -6,66 +6,34 @@ import static java.sql.Types.NULL;
  * Created by Leo Yoon on 13/10/2016.
  */
 public class User {
-    private String userName;
-    private String userPassword;
-    private String userType;
-    private String userFirstName;
-    private String userLastName;
-    private Integer userPhoneNumber;
+    //Boolean userType;
+    //Integer userID;
 
-    public User(String userName) {
-        this.userName = userName;
-        this.userPassword = new String();
-        this.userFirstName = new String();
-        this.userLastName = new String();
-        this.userPhoneNumber = new Integer(NULL);
+    String username;
+    String firstName;
+    String lastName;
+    String phoneNumber;
+
+    public User(String username, String firstName, String lastName, String phoneNumber) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
     }
 
-    public String getName() {
-        return this.userName;
+    public String getUsername(){
+        return this.username;
     }
 
-    public void setName(String userName) {
-        this.userName = userName;
+    public String getFirstName(){
+        return this.firstName;
     }
 
-    public String getPassword() {
-        return this.userPassword;
+    public String getLastName(){
+        return this.lastName;
     }
 
-    public void setPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
-
-    public String getUserType(String userType) {
-        return this.userType;
-    }
-
-    public void setUserFirstName(String userFirstName) {
-        this.userFirstName = userFirstName;
-    }
-
-    public String getUserFirstName() {
-        return this.userFirstName;
-    }
-
-    public void setUserLastName(String userLastName) {
-        this.userLastName = userLastName;
-    }
-
-    public String getUserLastName() {
-        return this.userLastName;
-    }
-
-    public void setUserPhoneNumber(Integer phoneNumber) {
-        this.userPhoneNumber = phoneNumber;
-    }
-
-    public Integer getUserPhoneNumber() {
-        return this.userPhoneNumber;
+    public String getPhoneNumber(){
+        return this.phoneNumber;
     }
 }
