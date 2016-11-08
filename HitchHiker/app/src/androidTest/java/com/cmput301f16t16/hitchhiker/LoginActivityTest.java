077a50d20cb1b0fcc6bcd1231d15cb00f046f9e2
leoyoon17 <l.yoon17@mgmail.com>
2016-11-07 17:22:30 -0700

    Almost finished implementing singleton, need to fix getContext() problem in UserListManagerTest.java

---
+++ HitchHiker/app/src/androidTest/java/com/cmput301f16t16/hitchhiker/LoginActivityTest.java
@@ -0,0 +1,36 @@
+package com.cmput301f16t16.hitchhiker;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by Leo Yoon on 07/11/2016.
+ */
+
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+    private Solo solo;
+
+    public LoginActivityTest() {
+        super(com.cmput301f16t16.hitchhiker.LoginActivity.class);
+    }
+
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+    public void setUp() throws Exception {
+        Log.d("TAG1", "setUp()");
+        solo = new Solo(getInstrumentation(),getActivity());
+    }
+
+    public void testLogin() {
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+
+        solo.enterText((EditText) solo.getView(R.id.userNameText), "test@email.com");
+        solo.enterText((EditText) solo.getView(R.id.passwordText), "testpassword");
+    }
+}
--- HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/RegisterActivity.java
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/RegisterActivity.java
@@ -3,11 +3,11 @@ package com.cmput301f16t16.hitchhiker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
