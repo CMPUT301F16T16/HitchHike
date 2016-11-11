7f2f747ca10b83261f4e74d07ca810d5931ad9d8
liaowilly12123 <liaowilly@hotmail.com>
2016-11-10 21:23:44 -0800

    Added new TextView into LoginActivity.

--- HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/LoginActivity.java
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.cmput301f16t16.hitchhiker;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,20 +19,22 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void GoToMainPage(View view){
-        // TODO get user from Elastic Search
+
+        // Get username the user has inputted.
         EditText userNameText = (EditText) findViewById(R.id.userNameText);
         String userName = userNameText.getText().toString();
 
-
+        // Goes into elasticsearch to try and find if the username exists
         ElasticsearchUserController.GetUserTask getUserTask = new ElasticsearchUserController.GetUserTask();
         getUserTask.execute(userName);
 
         try {
+            // return the user object in which the username corresponds to
             user = getUserTask.get();
             Log.i("User Name : ", user.getUserName());
             Log.i("User Type: ", user.getUserType().toString());
 
-            // TODO If the user is a rider, then go to the rider page
+            // If the user is a rider, then go to the rider page
             if (user.getUserType() == 1) {
 
                 Intent intent = new Intent(LoginActivity.this, RiderActivity.class);
@@ -39,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
 
-            // Todo If the user is a driver, then go to the driver page
+            // If the user is a driver, then go to the driver page
             else if (user.getUserType() == 2 || user.getUserType() == 3) {
                 Intent intent = new Intent(LoginActivity.this, DriverActivity.class);
                 intent.putExtra("user", user);
@@ -49,6 +53,9 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         catch (Exception e) {
+            TextView userDoesNotExistText = (TextView) findViewById(R.id.userDoesNotExistText);
+            userDoesNotExistText.setText("The username you have entered does not exist.\n");
+            userDoesNotExistText.setTextColor(Color.RED);
             Log.i("Error", "Failed to get the user out of the async object");
         }
 
