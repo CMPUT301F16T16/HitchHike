a36751ca7cacbed9dd67a79c6922d9de35867cdf
aabels <aabels@ualberta.ca>
2016-11-14 08:51:15 -0700

    Might Have to change gradle dependencies

---
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/LocationViewActivity.java
@@ -0,0 +1,199 @@
+package com.cmput301f16t16.hitchhiker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.*;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationViewActivity extends Activity {
+
+    private Request request;
+    private String requestId;
+
+    private LocationManager lm;
+    private String towers;
+    private int lat, longi;
+    private GeoPoint currentPoint;
+    Activity ourActivity = this;
+    MapView map;
+    Road[] mRoads;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_view);
+
+
+        request = (Request) getIntent().getSerializableExtra("request");
+
+//        Request request = requestsList.get(index);
+        requestId = request.getId();
+        String requestView = request.getTrip();
+        TextView displayTrip = (TextView) findViewById(R.id.loc_display_req_textview);
+        displayTrip.setText(requestView);
+
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
+        GeoPoint endPoint = new GeoPoint(48.4, -1.9);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(startPoint);
+
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria crit = new Criteria();
+        towers = lm.getBestProvider(crit, false);
+        android.location.Location location = lm.getLastKnownLocation(towers);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(LocationViewActivity.this, "First enable LOCATION ACCESS in settings.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (location != null) {
+            lat = (int) (location.getLatitude() * 1E6);
+            longi = (int) (location.getLongitude() * 1E6);
+            currentPoint = new GeoPoint(lat, longi);
+            setMarker(currentPoint);
+            mapController.animateTo(currentPoint);
+
+
+        } else {
+            Toast.makeText(LocationViewActivity.this, "Couldn't get provider", Toast.LENGTH_SHORT).show();
+        }
+
+
+        // http://stackoverflow.com/questions/38539637/osmbonuspack-roadmanager-networkonmainthreadexception
+        // accessed on October 27th, 2016
+        // author: yubaraj poudel
+        ArrayList<OverlayItem> overlayItemArray;
+        overlayItemArray = new ArrayList<>();
+
+        overlayItemArray.add(new OverlayItem("Starting Point", "This is the starting point", startPoint));
+        overlayItemArray.add(new OverlayItem("Destination", "This is the detination point", endPoint));
+//        getRoadAsync(startPoint, endPoint);
+    }
+
+    public void setMarker(GeoPoint sp) {
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(sp);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().clear();
+        map.getOverlays().add(startMarker);
+        map.invalidate();
+    }
+    public void getRoadAsync(GeoPoint startPoint, GeoPoint destinationPoint) {
+        mRoads = null;
+
+        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>(2);
+        waypoints.add(startPoint);
+        waypoints.add(destinationPoint);
+        new UpdateRoadTask().execute(waypoints);
+    }
+
+    private class UpdateRoadTask extends AsyncTask<Object, Void, Road[]> {
+
+        protected Road[] doInBackground(Object... params) {
+            @SuppressWarnings("unchecked")
+            ArrayList<GeoPoint> waypoints = (ArrayList<GeoPoint>) params[0];
+            RoadManager roadManager = new OSRMRoadManager(ourActivity);
+//            RoadManager roadManager = new MapQuestRoadManager("L1fY0M61AxWmso7ZUmsjZEtILXjzU3A6");
+            return roadManager.getRoads(waypoints);
+        }
+
+        @Override
+        //recieved help by looking at https://github.com/CMPUT301F16T01/Carrier/blob/master/app/src/main/java/comcmput301f16t01/github/carrier/ViewLocationsActivity.java
+        protected void onPostExecute(Road[] roads) {
+            mRoads = roads;
+            Road path = null;
+            double min = 0;
+            if (roads == null)
+                return;
+            if (roads[0].mStatus == Road.STATUS_TECHNICAL_ISSUE)
+                Toast.makeText(map.getContext(), "Technical issue when getting the route", Toast.LENGTH_SHORT).show();
+            else if (roads[0].mStatus > Road.STATUS_TECHNICAL_ISSUE) //functional issues
+                Toast.makeText(map.getContext(), "No possible route here", Toast.LENGTH_SHORT).show();
+            Polyline[] mRoadOverlays = new Polyline[roads.length];
+            List<Overlay> mapOverlays = map.getOverlays();
+//            for (int i = 0; i < roads.length; i++) {
+            for (Road road: roads) {
+                if(road.mLength < min || min == 0){
+                    min = road.mLength;
+                    path = road;
+                }
+            }
+            String routeDesc = path.getLengthDurationText(ourActivity, -1);
+//            bundle.putDouble("distance", path.mLength);
+//            bundle.putDouble("duration", path.mDuration);
+
+            Polyline roadPolyline = RoadManager.buildRoadOverlay(path);
+            roadPolyline.setTitle(getString(R.string.app_name) + " - " + routeDesc);
+            roadPolyline.setInfoWindow(new BasicInfoWindow(org.osmdroid.bonuspack.R.layout.bonuspack_bubble, map));
+            mapOverlays.add(0, roadPolyline);
+            map.invalidate();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    public GeoPoint getStartPoint() {
+//        return startPoint;
+//    }
+//
+//    public GeoPoint getEndPoint() {
+//        return endPoint;
+//    }
+//
+//    public GeoPoint getCurrentPoint() {
+//        return currentPoint;
+//    }
+//}
+}
