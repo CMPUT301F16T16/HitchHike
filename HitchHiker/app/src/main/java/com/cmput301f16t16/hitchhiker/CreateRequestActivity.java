078caa8c0a18362edfd52eed8b914f67bacf021f
leevtori <vlee2@ualberta.ca>
2016-11-23 15:41:32 -0700

    map is working, implementing this for the createactivity, gradle & api are all changed

--- HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/CreateRequestActivity.java
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/CreateRequestActivity.java
@@ -1,22 +1,49 @@
 package com.cmput301f16t16.hitchhiker;
 
+import android.graphics.Color;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+
 /**
  * The type Create request activity.
  */
-public class CreateRequestActivity extends AppCompatActivity {
+public class CreateRequestActivity extends AppCompatActivity implements OnMapReadyCallback {
     private User user;
+
+    private GoogleMap googleMap;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_request);
         user = (User) getIntent().getSerializableExtra("user");
+        mapView = (MapView) this.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+
     }
 
     /**
@@ -63,4 +90,113 @@ public class CreateRequestActivity extends AppCompatActivity {
             }
         }
     }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+        googleMap = mMap;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        // move camera to edmonton
+
+        //LatLng defaultLocation = new LatLng(53.5444,-113.4904);
+
+        // 51°03′N 114°04′W
+        double x1 = 51.03;
+        double y1 = -114.04;
+        double x2 = 53.54;
+        double y2 = -113.49;
+
+        LatLng startPoint = new LatLng(x1,y1);
+        LatLng endPoint = new LatLng(x2,y2);
+
+        // zoom to the mid point of the two coordinates dynamically
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(startPoint);
+        builder.include(endPoint);
+        LatLngBounds bound = builder.build();
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bound,600,600,5));
+
+        //make a marker
+
+        Marker start = googleMap.addMarker(new MarkerOptions()
+                .position(startPoint)
+                .title("Start Point")
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        );
+        //start.setTag(0);
+        Marker destination = googleMap.addMarker(new MarkerOptions()
+                .position(endPoint)
+                .title("End Point")
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        );
+        //destination.setTag(0);
+
+        // routing
+        // http://stackoverflow.com/questions/14444228/android-how-to-draw-route-directions-google-maps-api-v2-from-current-location-t
+        GMapV2Direction md = new GMapV2Direction();
+
+        Document doc = null;
+        doc = md.getDocument(startPoint,endPoint,
+                GMapV2Direction.MODE_DRIVING);
+        if (doc == null){
+            Toast.makeText(this,"Null",Toast.LENGTH_LONG).show();
+        }
+
+        ArrayList<LatLng> directionPoint = md.getDirection(doc);
+        PolylineOptions rectLine = new PolylineOptions().width(10).color(
+                Color.CYAN);
+
+        for (int i = 0; i < directionPoint.size(); i++) {
+            rectLine.add(directionPoint.get(i));
+        }
+
+        Polyline polylin = googleMap.addPolyline(rectLine);
+
+
+
+
+
+        //LatLng sydney = new LatLng(-34, 151);
+        //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation,10));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
 }
---
+++ HitchHiker/app/src/main/java/com/cmput301f16t16/hitchhiker/GMapV2Direction.java
@@ -0,0 +1,318 @@
+package com.cmput301f16t16.hitchhiker;
+
+/**
+ * Created by V1CTORIA2LEE on 2016-11-23.
+ */
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * The type G map v 2 direction.
+ * This class will get the route between two locations and draw a line
+ * http://stackoverflow.com/questions/14444228/android-how-to-draw-route-directions-google-maps-api-v2-from-current-location-t
+ * Accessed 21/11/16
+ */
+public class GMapV2Direction {
+
+     // The constant MODE_DRIVING.
+    public final static String MODE_DRIVING = "driving";
+     // The constant MODE_WALKING.
+    public final static String MODE_WALKING = "walking";
+
+    /**
+     * Instantiates a new G map v 2 direction.
+     */
+    public GMapV2Direction() {
+    }
+
+    /**
+     * Gets document.
+     *
+     * @param start the start
+     * @param end   the end
+     * @param mode  the mode
+     * @return the document
+     */
+    public Document getDocument(LatLng start, LatLng end, String mode) {
+        String url = "http://maps.googleapis.com/maps/api/directions/xml?"
+                + "origin=" + start.latitude + "," + start.longitude
+                + "&destination=" + end.latitude + "," + end.longitude
+                + "&sensor=false&units=metric&mode=driving";
+        Log.d("url", url);
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpPost httpPost = new HttpPost(url);
+            HttpResponse response = httpClient.execute(httpPost, localContext);
+            InputStream in = response.getEntity().getContent();
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+            Document doc = builder.parse(in);
+            return doc;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.print(e.getMessage()+"---------------------------------------------");
+        }
+        return null;
+    }
+
+    /**
+     * Gets duration text.
+     *
+     * @param doc the doc
+     * @return the duration text
+     */
+    public String getDurationText(Document doc) {
+        try {
+
+            NodeList nl1 = doc.getElementsByTagName("duration");
+            Node node1 = nl1.item(0);
+            NodeList nl2 = node1.getChildNodes();
+            Node node2 = nl2.item(getNodeIndex(nl2, "text"));
+            Log.i("DurationText", node2.getTextContent());
+            return node2.getTextContent();
+        } catch (Exception e) {
+            return "0";
+        }
+    }
+
+    /**
+     * Gets duration value.
+     *
+     * @param doc the doc
+     * @return the duration value
+     */
+    public int getDurationValue(Document doc) {
+        try {
+            NodeList nl1 = doc.getElementsByTagName("duration");
+            Node node1 = nl1.item(0);
+            NodeList nl2 = node1.getChildNodes();
+            Node node2 = nl2.item(getNodeIndex(nl2, "value"));
+            Log.i("DurationValue", node2.getTextContent());
+            return Integer.parseInt(node2.getTextContent());
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    /**
+     * Gets distance text.
+     *
+     * @param doc the doc
+     * @return the distance text
+     */
+    public String getDistanceText(Document doc) {
+        /*
+         * while (en.hasMoreElements()) { type type = (type) en.nextElement();
+         *
+         * }
+         */
+
+        try {
+            NodeList nl1;
+            nl1 = doc.getElementsByTagName("distance");
+
+            Node node1 = nl1.item(nl1.getLength() - 1);
+            NodeList nl2 = null;
+            nl2 = node1.getChildNodes();
+            Node node2 = nl2.item(getNodeIndex(nl2, "value"));
+            Log.d("DistanceText", node2.getTextContent());
+            return node2.getTextContent();
+        } catch (Exception e) {
+            return "-1";
+        }
+
+        /*
+         * NodeList nl1; if(doc.getElementsByTagName("distance")!=null){ nl1=
+         * doc.getElementsByTagName("distance");
+         *
+         * Node node1 = nl1.item(nl1.getLength() - 1); NodeList nl2 = null; if
+         * (node1.getChildNodes() != null) { nl2 = node1.getChildNodes(); Node
+         * node2 = nl2.item(getNodeIndex(nl2, "value")); Log.d("DistanceText",
+         * node2.getTextContent()); return node2.getTextContent(); } else return
+         * "-1";} else return "-1";
+         */
+    }
+
+    /**
+     * Gets distance value.
+     *
+     * @param doc the doc
+     * @return the distance value
+     */
+    public int getDistanceValue(Document doc) {
+        try {
+            NodeList nl1 = doc.getElementsByTagName("distance");
+            Node node1 = null;
+            node1 = nl1.item(nl1.getLength() - 1);
+            NodeList nl2 = node1.getChildNodes();
+            Node node2 = nl2.item(getNodeIndex(nl2, "value"));
+            Log.i("DistanceValue", node2.getTextContent());
+            return Integer.parseInt(node2.getTextContent());
+        } catch (Exception e) {
+            return -1;
+        }
+        /*
+         * NodeList nl1 = doc.getElementsByTagName("distance"); Node node1 =
+         * null; if (nl1.getLength() > 0) node1 = nl1.item(nl1.getLength() - 1);
+         * if (node1 != null) { NodeList nl2 = node1.getChildNodes(); Node node2
+         * = nl2.item(getNodeIndex(nl2, "value")); Log.i("DistanceValue",
+         * node2.getTextContent()); return
+         * Integer.parseInt(node2.getTextContent()); } else return 0;
+         */
+    }
+
+    /**
+     * Gets start address.
+     *
+     * @param doc the doc
+     * @return the start address
+     */
+    public String getStartAddress(Document doc) {
+        try {
+            NodeList nl1 = doc.getElementsByTagName("start_address");
+            Node node1 = nl1.item(0);
+            Log.i("StartAddress", node1.getTextContent());
+            return node1.getTextContent();
+        } catch (Exception e) {
+            return "-1";
+        }
+
+    }
+
+    /**
+     * Gets end address.
+     *
+     * @param doc the doc
+     * @return the end address
+     */
+    public String getEndAddress(Document doc) {
+        try {
+            NodeList nl1 = doc.getElementsByTagName("end_address");
+            Node node1 = nl1.item(0);
+            Log.i("StartAddress", node1.getTextContent());
+            return node1.getTextContent();
+        } catch (Exception e) {
+            return "-1";
+        }
+    }
+
+    /**
+     * Gets copy rights.
+     *
+     * @param doc the doc
+     * @return the copy rights
+     */
+    public String getCopyRights(Document doc) {
+        try {
+            NodeList nl1 = doc.getElementsByTagName("copyrights");
+            Node node1 = nl1.item(0);
+            Log.i("CopyRights", node1.getTextContent());
+            return node1.getTextContent();
+        } catch (Exception e) {
+            return "-1";
+        }
+
+    }
+
+    /**
+     * Gets direction.
+     *
+     * @param doc the doc
+     * @return the direction
+     */
+    public ArrayList<LatLng> getDirection(Document doc) {
+        NodeList nl1, nl2, nl3;
+        ArrayList<LatLng> listGeopoints = new ArrayList<LatLng>();
+        nl1 = doc.getElementsByTagName("step");
+        if (nl1.getLength() > 0) {
+            for (int i = 0; i < nl1.getLength(); i++) {
+                Node node1 = nl1.item(i);
+                nl2 = node1.getChildNodes();
+
+                Node locationNode = nl2
+                        .item(getNodeIndex(nl2, "start_location"));
+                nl3 = locationNode.getChildNodes();
+                Node latNode = nl3.item(getNodeIndex(nl3, "lat"));
+                double lat = Double.parseDouble(latNode.getTextContent());
+                Node lngNode = nl3.item(getNodeIndex(nl3, "lng"));
+                double lng = Double.parseDouble(lngNode.getTextContent());
+                listGeopoints.add(new LatLng(lat, lng));
+
+                locationNode = nl2.item(getNodeIndex(nl2, "polyline"));
+                nl3 = locationNode.getChildNodes();
+                latNode = nl3.item(getNodeIndex(nl3, "points"));
+                ArrayList<LatLng> arr = decodePoly(latNode.getTextContent());
+                for (int j = 0; j < arr.size(); j++) {
+                    listGeopoints.add(new LatLng(arr.get(j).latitude, arr
+                            .get(j).longitude));
+                }
+
+                locationNode = nl2.item(getNodeIndex(nl2, "end_location"));
+                nl3 = locationNode.getChildNodes();
+                latNode = nl3.item(getNodeIndex(nl3, "lat"));
+                lat = Double.parseDouble(latNode.getTextContent());
+                lngNode = nl3.item(getNodeIndex(nl3, "lng"));
+                lng = Double.parseDouble(lngNode.getTextContent());
+                listGeopoints.add(new LatLng(lat, lng));
+            }
+        }
+
+        return listGeopoints;
+    }
+
+    private int getNodeIndex(NodeList nl, String nodename) {
+        for (int i = 0; i < nl.getLength(); i++) {
+            if (nl.item(i).getNodeName().equals(nodename))
+                return i;
+        }
+        return -1;
+    }
+
+    private ArrayList<LatLng> decodePoly(String encoded) {
+        ArrayList<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng position = new LatLng((double) lat / 1E5, (double) lng / 1E5);
+            poly.add(position);
+        }
+        return poly;
+    }
+}
