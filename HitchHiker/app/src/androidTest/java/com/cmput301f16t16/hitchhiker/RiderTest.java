7af273022cc1425ee18832e5fe0efa736a8644dc
aabels <aabels@ualberta.ca>
2016-11-26 22:58:27 -0700

    fixed user phone to string and added Tests/Fixed OldOnes

--- HitchHiker/app/src/androidTest/java/com/cmput301f16t16/hitchhiker/RiderTest.java
+++ HitchHiker/app/src/androidTest/java/com/cmput301f16t16/hitchhiker/RiderTest.java
@@ -1,3 +1,7 @@
+package com.cmput301f16t16.hitchhiker;
+
+import junit.framework.TestCase;
+
 //package com.cmput301f16t16.hitchhiker;
 //
 //import junit.framework.Test;
@@ -7,22 +11,5 @@
 // * Created by Jae-yeon on 10/14/2016.
 // */
 //
-//public class RiderTest extends TestCase {
-//
-//    public void testCreateRider(){
-//        String userName = "Stitch";
-//        String userFirstName = "Willy";
-//        String userLastName = "Liao";
-//        String userPhoneNumber = "1234567890";
-//
-//        Rider newRider = new Rider(userName, userFirstName, userLastName, userPhoneNumber, );
-//
-//        //Test to see if values passed are creqted and stored. It tests for equality.
-//        assertEquals("Username is not the same", userName, newRider.getUserName());
-//        assertEquals("First name is not the same", userFirstName, newRider.getUserFirstName());
-//        assertEquals("Last name is not the same", userLastName, newRider.getUserLastName());
-//        assertEquals("Phone number is not the same", userPhoneNumber, newRider.getUserPhoneNumber());
-//    }
-//
-//
-//}
+
+
